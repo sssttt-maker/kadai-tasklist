d99b252c580ee1272d7f7b5cc777df4f8f424281
shuhei.takai <stst.shu82@gmail.com>
2019-08-02 07:30:04 +0000

    correct code

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+     before_action :require_user_logged_in, only: [:index, :show, :new, :edit]
+
     # before_action :correct_user, only: [:destroy]
     
     
